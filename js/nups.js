d3293c9f024887649a68c00a1dbf854ee8843c81
Jack Lowrie <jblowrie@u.northwestern.edu>
2014-03-13 17:32:20 -0500

    Started on Messageboard
    —Made message board div
    
    —started styling and implementing through query

--- js/nups.js
+++ js/nups.js
@@ -1,5 +1,17 @@
-game = new Object();
-game.time="3:00pm";
-game.type="Basketball"
-game.location="SPAC"
-game.style="intense"
+$(document).ready( function(){
+
+//Creating a New Game
+	game = new Object();
+	game.time="3:00pm";
+	game.type="Basketball";
+	game.location="SPAC";
+	game.style="intense";
+
+
+// Post Message Button
+	$("#postmessage").click(function(){
+		var inputVal = $("#mymessage").val();
+		("messageboard").append( $("<div>inputVal</div>").addClass("new-message") );
+	});
+
+});
